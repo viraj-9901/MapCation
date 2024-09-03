f72975550ca3baff3643f16467253b478fb6b586
Viraj kanjia <virajkanjia9@gmail.com>
2024-09-03 22:17:48 +0530

    Initial commit and setup and bind sanity studio with nextJS

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sanity-mapcation',
+
+  projectId: '9syhq647',
+  dataset: 'production',
+  basePath: "/studio",
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
